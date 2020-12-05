ecb76d8905f49e82503b5305b8d34f872c916998
Steven Hickson <me@stevenhickson.com>
2020-12-05 09:42:07 -0800

    Adding script to generate raspimons from an image. Updating bestiary

--- My_Raspimons/bestiary.py
+++ My_Raspimons/bestiary.py
@@ -1,5 +1,6 @@
 from sense_hat import SenseHat
 from time import sleep
+from collections import Counter
 
 sense = SenseHat()
 
@@ -56,12 +57,43 @@ raspee =[k, w, w, d, d, d, k, k,
          k, k, n, n, n, n, k, k,
          n, c, c, y, y, o, c, p,
          k, p, p, k, p, p, k, k
-]       
+]
 
-raspimons = [ raspee, chirp, frankie, codey]
+wubby = [k, k, d, d, d, d, k, k,
+         k, d, d, w, d, w, d, k,
+         d, d, d, k, d, k, d, d,
+         d, d, r, d, d, d, r, d,
+         p, p, d, d, k, d, d, p,
+         k, r, r, d, d, d, p, k,
+         r, r, r, r, p, p, p, p,
+         k, k, k, k, k, k, k, k
+]
 
+sullie = [ w,o,w,w,w,w,o,w,w,c,c,c,c,c,c,w,w,c,c,c,c,c,c,w,c,c,k,w,w,k,c,w,c,b,c,k,k,c,b,c,c,c,c,c,c,c,c,w,w,w,c,c,c,c,w,w,w,b,c,w,w,c,b,w ]
+
+volt = [ p,p,r,r,r,r,p,p,p,r,r,r,r,r,r,p,r,w,r,r,r,w,r,r,r,w,k,r,w,w,r,r,r,r,r,r,r,r,r,r,w,w,w,w,w,w,w,w,p,w,w,w,w,w,w,p,p,p,w,w,w,w,p,p ]
+
+# This function will find the most common color from a raspimon
+def most_common_color(raspimon):
+    # let's get the occurences, a dictionary of color values paired with the number of times they show up.
+    occurrences = Counter(raspimon)
+    #print(occurrences)
+    # Let's find the most common occurence that's not k (empty color)
+    top_count = 0
+    top_color = k
+    for color, count in occurrences.items():
+        if count > top_count and color != k:
+            top_count = count
+            top_color = color
+    #print(top_color)
+    return top_color
+
+
+raspimons = [ raspee, chirp, frankie, codey, wubby, sullie, volt]
+names = ['raspee', 'chirp', 'frankie', 'codey', 'wubby', 'sullie', 'volt']
 while True:
-    for raspimon in raspimons:
+    for name, raspimon in zip(names, raspimons):
+        sense.show_message(name, text_colour=most_common_color(raspimon), scroll_speed=0.08)
         sense.set_pixels(raspimon)
         sleep(2)    
         
