0037182188ea54505db4581fdea6b21fb27867f7
Code Next <61989260+CodeNextAdmin@users.noreply.github.com>
2020-12-07 06:02:32 -0800

    added solution

--- Stage1/raspimon_makeover.py
+++ Stage1/raspimon_makeover.py
@@ -13,8 +13,13 @@ sense.show_message(names[1])
 
 #Colors:
 
-r = (255, 0, 0 ) # RED color, stored in an another data structure called a tuple.
-k = (0, 0, 0) # black means zero amounts of red, green and blue
+r = (255, 0, 0) # red to max value, no green or blue
+w = (255, 255, 255) #white is all colors maxed
+k = (0, 0, 0) #RGB black, but for LED it means off
+g = (0, 255, 0)
+l = (128, 255, 128) #lightGreen
+y = (255, 255, 0) #yellow
+o = (255, 128, 0) #orange
 
 #define another color. Use one letter variable names to make it easy later
 
@@ -32,7 +37,20 @@ k, k, r, k, r, k, k, k
 sense.set_pixels(raspimon)
 
 #add a one-pixel mouth
-
+sense.set_pixel(3,4, [255,0,0])
  
+sleep(1.5)
+sense.show_message("changing form...")
 
+chirp1 = [k, k, k, k, k, k, k, k,
+          k, k, k, l, r, y, k, k,
+          k, k, k, l, l, k, k, k,
+          l, g, g, w, w, g, g, l,
+          k, l, g, w, w, l, g, k,
+          k, k, l, w, w, l, k, k,
+          k, k, k, l, l, k, k, k,
+          k, k, o, k, k, o, k, k]
+
+   
+sense.set_pixels(chirp1)
 
