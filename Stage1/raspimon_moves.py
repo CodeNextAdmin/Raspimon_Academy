6d32ef5646442142da0845c75ddcbb7eb817dccc
Code Next <61989260+CodeNextAdmin@users.noreply.github.com>
2020-12-07 12:50:08 -0800

    added solution

--- Stage1/raspimon_moves.py
+++ Stage1/raspimon_moves.py
@@ -17,31 +17,48 @@ p = (128, 0, 128) #purple
 d = (255, 0, 128) #darkPink
 l = (128, 255, 128) #lightGreen
 
-open_eyes = [k, k, d, d, d, d, k, k,
-             k, d, d, w, d, w, d, k,
-             d, d, d, k, d, k, d, d,
-             d, d, r, d, d, d, r, d,
-             p, p, d, d, k, d, d, p,
-             k, r, r, d, d, d, p, k,
-             r, r, r, r, p, p, p, p,
-             k, k, k, k, k, k, k, k ]       
+raspimon = [
+k, k, d, d, d, d, k, k,
+k, d, d, w, d, w, d, k,
+d, d, d, k, d, k, d, d,
+d, d, r, d, d, d, r, d,
+p, p, d, d, k, d, d, p,
+k, r, r, d, d, d, p, k,
+r, r, r, r, p, p, p, p,
+k, k, k, k, k, k, k, k
+]       
 
-closed_eyes =[k, k, d, d, d, d, k, k,
-              k, d, d, d, d, d, d, k,
-              d, d, d, d, d, d, d, d,
-              d, d, r, d, d, d, r, d,
-              p, p, d, d, k, d, d, p,
-              k, r, r, d, d, d, p, k,
-              r, r, r, r, p, p, p, p,
-              k, k, k, k, k, k, k, k ]
+raspimon2 =[
+k, k, d, d, d, d, k, k,
+k, d, d, d, d, d, d, k,
+d, d, d, d, d, d, d, d,
+d, d, r, d, d, d, r, d,
+p, p, d, d, k, d, d, p,
+k, r, r, d, d, d, p, k,
+r, r, r, r, p, p, p, p,
+k, k, k, k, k, k, k, k
+]
 
-sense.set_pixels(open_eyes)
+sense.set_pixels(raspimon)
 
 #Create animation below
-
-
-
-
-
-print('Done')
-
+sense.set_pixels(raspimon2)
+sleep(.2)
+sense.set_pixels(raspimon2)
+sleep(.2)
+sense.set_pixels(raspimon)
+sleep(.3)
+sense.set_pixels(raspimon2)
+sleep(.2)
+sense.set_pixels(raspimon)
+sleep(.3)
+sense.set_pixels(raspimon2)
+sleep(.2)
+sense.set_pixels(raspimon)
+sleep(.3)
+sense.flip_h()
+sleep(.4)
+sense.flip_h()
+sleep(.4)
+sense.flip_h()
+sleep(.4)
