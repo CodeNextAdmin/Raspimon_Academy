c17746fc9a58da1b40187aa6d16f08ec095ae014
Code Next <61989260+CodeNextAdmin@users.noreply.github.com>
2020-12-08 17:35:15 -0800

    added solution

--- Stage2/raspimon_tap.py
+++ Stage2/raspimon_tap.py
@@ -33,9 +33,33 @@ tapped = [
 sense.set_pixels(rest) #set your own Raspimons here as default rest pose
 
 #add a while True: loop below
-        
+while True:
+    accel = sense.get_accelerometer_raw()
+    sense.set_pixels(rest) #replace with your own
 
-
+    x = accel["x"] #get each separate float value
+    y = accel["y"]
+    z = accel["z"]
+    
+    #changes all negative values to positive
+    x = abs(x)
+    y = abs(y)
+    z = abs(z)
+    
+    #round to 3 dec. values
+    x = round(x, 3)  
+    y = round(x, 3)
+    
+    #the sum simplifies the detection of taps.  
+    sum = x+y
+    print(str(sum))
+     
+    #the value will need to be tested. When you get a value that works, add it below.   
+    if sum > .1:
+        print("Tap")
+        sense.set_pixels(tapped)
+        sleep(.5)
+         
         
 
  
