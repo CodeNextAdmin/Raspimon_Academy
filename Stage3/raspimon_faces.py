f2e511bb4ccabbf1503975069af071f481e8380a
Code Next <61989260+CodeNextAdmin@users.noreply.github.com>
2020-12-09 15:15:48 -0500

    Update raspimon_faces.py

--- Stage3/raspimon_faces.py
+++ Stage3/raspimon_faces.py
@@ -3,7 +3,7 @@ from time import sleep
 import vision_lib
 
 #initialize your SenseHat instance below here
-
+sense = SenseHat()
 
 #paste your colors & Raspimon below here
 
@@ -15,7 +15,7 @@ import vision_lib
 def see_emotion(emotion):
     print(emotion)
     #your code below here
-
+    sense.show_message(emotion)
 
 
 if __name__ == '__main__':
