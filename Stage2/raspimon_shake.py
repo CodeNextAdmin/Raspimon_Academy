bc7fa18b06c7d20d9c3ff1ce38a4906b87572e2e
Code Next <61989260+CodeNextAdmin@users.noreply.github.com>
2020-12-09 13:11:52 -0800

    added solution

--- Stage2/raspimon_shake.py
+++ Stage2/raspimon_shake.py
@@ -20,6 +20,7 @@ rest = [
         k, k, w, k, w, k, k, k,
         k, k, w, k, w, k, k, k
         ]
+
 shake_left = [
         k, k, k, k, k, k, k, k,
         w, w, w, w, w, w, k, k,
@@ -42,16 +43,50 @@ shake_right = [
         k, k, k, w, k, w, k, k
         ]
 
+
 sense.set_pixels(shake_right) #set your own Raspimons here as default rest pose
 
 #define your shake() function here:
-
+def shake():
+    print("shaking")
+    sense.set_pixels(shake_left)
+    sleep(.2)
+    sense.set_pixels(shake_right)
+    sleep(.2)
+    sense.set_pixels(shake_left)
+    sleep(.2)
+    sense.set_pixels(shake_right)
+    sleep(.2)
+    sense.set_pixels(shake_left)
+    sleep(.2)
+    sense.set_pixels(shake_right)
 
     
 #add your while True: loop below
-        
+while True:
+    accel = sense.get_accelerometer_raw()
+    sense.set_pixels(rest) #replace with your own
 
-        
-
+    x = accel["x"] #get each separate float value
+    y = accel["y"]
+    z = accel["z"]
+    
+    #changes all negative values to positive
+    x = abs(x)
+    y = abs(y)
+    z = abs(z)
+    
+    #round to 3 dec. values
+    x = round(x, 3)  
+    y = round(x, 3)
+    z = round(z, 3)
+    
+    #print( "x : " + str(x) + " y : " + str(y) + " z : " +str(z))
+    sum = x+y+z
+    print(str(sum))
+    
+    if sum > 3: 
+        print("shake")
+        shake()
 
 
